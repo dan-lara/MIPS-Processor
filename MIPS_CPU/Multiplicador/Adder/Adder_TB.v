1423a29f1318cdb48cba3d672dc866b40b76929b
Galdino <matheusgaldino2011@gmail.com>
2024-06-23 16:54:48 -0300

    Adder

---
+++ MIPS_CPU/Multiplicador/Adder/Adder_TB.v
@@ -0,0 +1,41 @@
+`timescale 1ns/100ps
+
+module Adder_TB();
+
+	reg [15:0] OperandoA, OperandoB;
+	wire [16:0] Soma;
+
+	Adder DUT (
+		.OperandoA(OperandoA), 
+		.OperandoB(OperandoB), 
+		.Soma(Soma)
+	);
+
+	initial begin
+		OperandoA = 0;
+		OperandoB = 0;
+		#10;
+		
+		OperandoA = 16'h0001;
+		OperandoB = 16'h0001;
+		#10;
+
+		OperandoA = 16'hFFFF;
+		OperandoB = 16'h0001;
+		#10;
+
+		OperandoA = 16'hFFFF;
+		OperandoB = 16'hFFFF;
+		#10;
+
+		OperandoA = 16'h8000;
+		OperandoB = 16'h8000;
+		#10;
+
+		$stop;
+	
+	end
+
+
+
+endmodule
