ef480052dbb763e9c6ba473143669da01f678f83
Daniel Lara <d2021003661@unifei.edu.br>
2024-06-26 12:15:58 -0300

    PC

--- MIPS_CPU/PC/pc.v
+++ MIPS_CPU/PC/pc.v
@@ -1,18 +1,13 @@
 module pc (
 	input reset, clk,
-	output reg [12:0] Address
+	output reg [11:0] Address
 );
 	
 	always @(posedge clk or posedge reset)begin 
-		if(reset)begin
+		if(reset)
 			Address <= 12'b1001_0100_0000; //Reseta para o endereço Grupo 4 * 0x250 = 0x940
-		end
-		else if(Address<12'b1101_0011_1111) begin
-			Address <= Address + 1'b1; //Contagem Incremental
-		end
-		else begin
-			Address <= 12'b1001_0100_0000; //Reseta para o endereço Grupo 4 * 0x250 = 0x940
-		end	
+		else
+			Address <= Address + 12'b1; //Contagem Incremental
 	end	
 endmodule
 
