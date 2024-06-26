8f16c740f31aacaa1d9ff749e5216332d8102ec6
Galdino <matheusgaldino2011@gmail.com>
2024-06-26 18:05:07 -0300

    Control

--- MIPS_CPU/Control/control.v
+++ MIPS_CPU/Control/control.v
@@ -1,90 +1,90 @@
 module control 
 (
-	input  [31:0] Instrucao, 
-	output [23:0] Controle
+	input  [31:0] instrucao, 
+	output [23:0] controle
 );
 
 	reg RW;                
 	reg [1:0] Operacao;         
-	reg Habilitar_Offset;     
-	reg MUX_ALU_Entrada;        
-	reg MUX_ALU_Saida;       
+	reg Offset_Enable;     
+	reg MUX_ALU_IN;        
+	reg MUX_ALU_OUT;       
 	reg MUX_WB;            
 	reg WR;                
-	reg Habilita_MULT;	
+	reg MULT_Enable;	
 	
-	// Registradores Rs, Rt e Rd
-	reg [4:0]  Rs;		     
-	reg [4:0]  Rt;		      			  
-	reg [4:0]  Rd;	        
+	// Registradores rs, rt e rd
+	reg [4:0]  rs;		     
+	reg [4:0]  rt;		      			  
+	reg [4:0]  rd;	        
 	
 	// Vetor que contém todas as intruções de controle necessárias.
-	assign Controle = {RW, Operacao, Habilitar_Offset, MUX_ALU_Entrada, MUX_ALU_Saida, MUX_WB, WR, Habilita_MULT, Rs, Rt, Rd}; 
+	assign controle = {RW, Operacao, Offset_Enable, MUX_ALU_IN, MUX_ALU_OUT, MUX_WB, WR, MULT_Enable, rs, rt, rd}; 
 	
-	always @(Instrucao) begin
-		Rs = Instrucao[25:21];
-		Rt = Instrucao[20:16];
+	always @(instrucao) begin
+		rs = instrucao[25:21];
+		rt = instrucao[20:16];
 		WR = 1;
-		Habilita_MULT = 0;
+		MULT_Enable = 0;
 		Operacao = 0;
-		MUX_ALU_Entrada = 0;    
-		MUX_ALU_Saida = 1;
-		Rd = 0;
+		MUX_ALU_IN = 0;    
+		MUX_ALU_OUT = 1;
+		rd = 0;
 		MUX_WB = 0;
-		Habilitar_Offset = 0;
+		Offset_Enable = 0;
 		RW = 0;
 		
 		// Instruções de formato i
-		if(Instrucao[31:26] == 32'b1111) begin // (grupo + 1) = 14 + 1 = 15 - LW 	
-			Habilita_MULT = 0;
+		if(instrucao[31:26] == 6'b000101) begin // (grupo + 1) = 4 + 1 = 5 - LW 	
+			MULT_Enable = 0;
 			RW = 1;            
 			Operacao = 0;           		
-			Habilitar_Offset = 1; 
-			MUX_ALU_Entrada = 1;    
-			MUX_ALU_Saida = 1;   
+			Offset_Enable = 1; 
+			MUX_ALU_IN = 1;    
+			MUX_ALU_OUT = 1;   
 			MUX_WB = 1;        
 			WR = 1;            
-			Rd = Rt; 											
+			rd = rt; 											
 		end
 	
-		if(Instrucao[31:26] == 32'b10000) begin // (grupo + 2) = 14 + 2 = 16 - SW
+		if(instrucao[31:26] == 6'b000110) begin // (grupo + 2) = 4 + 2 = 6 - SW
 			RW = 0;            
 			Operacao = 0;           		
-			Habilitar_Offset = 1; 
-			MUX_ALU_Entrada = 1;    
-			MUX_ALU_Saida = 1;   
+			Offset_Enable = 1; 
+			MUX_ALU_IN = 1;    
+			MUX_ALU_OUT = 1;   
 			MUX_WB = 1;        
 			WR = 0;             
-			Rd = 0;
+			rd = 0;
 		end
 	
 		// Instruções de formato R
-		if(Instrucao[31:26] == 32'b1110) begin // Grupo 14
-			Rd = Instrucao[15:11];
+		if(instrucao[31:26] == 6'b000100) begin // Grupo 4
+			rd = instrucao[15:11];
 			RW = 1;
-			Habilitar_Offset = 0;
-			MUX_ALU_Entrada = 0;
+			Offset_Enable = 0;
+			MUX_ALU_IN = 0;
 			MUX_WB = 0; 
 			WR = 1; 
-			if(Instrucao[10:6] == 10 && Instrucao[5:0] == 50) begin 			//MUL
-				Habilita_MULT = 1;
-				MUX_ALU_Saida = 0;
-			end else if(Instrucao[10:6] == 10 && Instrucao[5:0] == 32) begin //ADD	            
+			if(instrucao[10:6] == 10 && instrucao[5:0] == 50) begin 			//MUL
+				MULT_Enable = 1;
+				MUX_ALU_OUT = 0;
+			end else if(instrucao[10:6] == 10 && instrucao[5:0] == 32) begin //ADD	            
 				Operacao = 0; 
-				Habilita_MULT = 0;	
-				MUX_ALU_Saida = 1;        
-			end else if(Instrucao[10:6] == 10 && Instrucao[5:0] == 34) begin //SUB
+				MULT_Enable = 0;	
+				MUX_ALU_OUT = 1;        
+			end else if(instrucao[10:6] == 10 && instrucao[5:0] == 34) begin //SUB
 				Operacao = 1;
-				Habilita_MULT = 0;
-				MUX_ALU_Saida = 1;
-			end else if(Instrucao[10:6] == 10 && Instrucao[5:0] == 36) begin	//AND
+				MULT_Enable = 0;
+				MUX_ALU_OUT = 1;
+			end else if(instrucao[10:6] == 10 && instrucao[5:0] == 36) begin	//AND
 				Operacao = 2;
-				Habilita_MULT = 0;
-				MUX_ALU_Saida = 1;
-			end else if(Instrucao[10:6] == 10 && Instrucao[5:0] == 37) begin //OR
+				MULT_Enable = 0;
+				MUX_ALU_OUT = 1;
+			end else if(instrucao[10:6] == 10 && instrucao[5:0] == 37) begin //OR
 				Operacao = 3; 
-				Habilita_MULT = 0;
-				MUX_ALU_Saida = 1;
+				MULT_Enable = 0;
+				MUX_ALU_OUT = 1;
 			end
 		end
 	end
--- MIPS_CPU/Control/control_TB.v
+++ MIPS_CPU/Control/control_TB.v
@@ -1,30 +1,30 @@
 `timescale 1ns/100ps
-module 	();
+module control_TB();
 
 	// Sinais para simulação
-	reg  [31:0] Instrucao;
-	wire [23:0] Controle;
+	reg  [31:0] instrucao;
+	wire [23:0] controle;
 	
 	// Device Under Test
 	control DUT 
 	(
-		.Instrucao(Instrucao),
-		.Controle(Controle)
+		.instrucao(instrucao),
+		.controle(controle)
 	);
 	
 	
 	// Simulação dos sinais de controle para cada uma das instruções.
 	initial begin
-	
+		$monitor("Time = %0t : Instrucao = %b, Controle = %b", $time, instrucao, controle);
 		// Formato i:
-		Instrucao = 32'b001111_00000_00001_0100_0001_0000_0000; 	  //LW
-		#10 Instrucao = 32'b010000_00000_00001_0000_0000_0000_0000; //SW
+		    instrucao = 32'b000101_00000_00001_0100_0001_0000_0000; 	  //LW
+		#10 instrucao = 32'b000110_00000_00001_0000_0000_0000_0000; //SW
 		
 		
 		// Formato R:
-		#10 Instrucao = 32'b001110_00001_00010_00101_01010_110010;  //MUL
-		#10 Instrucao = 32'b001110_00011_00100_00110_01010_100000;  //ADD
-		#10 Instrucao = 32'b001110_00101_00110_00111_01010_100010;  //SUB
+		#10 instrucao = 32'b000100_00001_00010_00101_01010_110010;  //MUL
+		#10 instrucao = 32'b001110_00011_00100_00110_01010_100000;  //ADD
+		#10 instrucao = 32'b001110_00101_00110_00111_01010_100010;  //SUB
 		#10 $stop;
 	end
 	
